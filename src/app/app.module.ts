0be34d63bcd058c2a82ab9ecde154276682513fc
amit kandwal <amitkandwal416@gmail.com>
2018-11-26 23:25:24 +0530

    routing lazyloading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,17 @@ import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HtmlComponent } from './dashboard/html/html.component';
 import { CssComponent } from './dashboard/css/css.component';
 
 const routes: Routes = [
-  { path: '', component: HtmlComponent },
-  { path: 'html', component: HtmlComponent },
-  { path: 'css', component: CssComponent },
+  { path: '', loadChildren:'./admin/login/admin.module#AdminModule' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     DashboardComponent,
     HtmlComponent,
     CssComponent
